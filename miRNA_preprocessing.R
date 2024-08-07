17d693f826f4b26d5b1fd14301eb204f1c8fdcec
Ghada NOUAIRIA <nouairiaghada@gmail.com>
2024-08-07 09:00:31 +0200

    Adding hairpin miRNA

--- miRNA_preprocessing.R
+++ miRNA_preprocessing.R
@@ -8,7 +8,8 @@
 library(tidyverse)
 
 # Read the data
-data <- read_csv('data/miRNA_mature_log.csv')
+data1 <- read_csv('data/miRNA_mature_log.csv')
+data <- read_csv('data/miRNA_hairpin_log.csv')
 
 # Tidy the data
 data <- data %>%
@@ -47,4 +48,46 @@ data <- data %>%
   select_if(~ n_distinct(.x) > 1)
 
 # Save the data as .csv
-write_csv(data, "results/miRNA_preprocessed.csv")
+write_csv(data, "results/miRNA_hairpin_preprocessed.csv")
+
+# Tidy the data
+data1 <- data1 %>%
+  # Transposing the table so that each row represents a patient and each column represents a compound
+  pivot_longer(cols = c(-1), names_to = "patient_id") %>%
+  pivot_wider(names_from = c(1)) %>%
+  mutate(across('patient_id', str_replace, '_.*', '')) %>% 
+  # Create a numeric patient ID column
+  mutate(patient_id = as.numeric(patient_id)) %>% 
+  arrange(patient_id)
+
+# Evaluate the data
+## Checking for NA's
+data1 %>% 
+  is.na() %>%
+  sum()
+## Conclusion: There are no NA's.
+
+## Checking for constant columns
+data1 %>% 
+  dplyr::select(-1) %>% 
+  pivot_longer(
+    everything(), 
+    names_to = "variable", 
+    values_to = "value"
+  ) %>% 
+  group_by(variable) %>% 
+  summarise(distinct = n_distinct(value)) %>% 
+  filter(distinct == 1) %>% 
+  summarise(n = n())
+## Conclusion: There are 1187 constant columns.
+
+# Filter the data
+data1 <- data1 %>% 
+  # Removing columns with constant values
+  select_if(~ n_distinct(.x) > 1)
+
+
+###joining miRNA data from hairpin and mature
+data %>%
+ inner_join(data1, by = "patient_id") %>%
+  write_csv("results/miRNA_preprocessed.csv")
