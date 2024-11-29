285b106014e4341d3f8fe83e0f9f7c1b78f125ca
Ghada NOUAIRIA <nouairiaghada@gmail.com>
2024-11-29 08:50:30 +0100

    Add files via upload

--- metabolite_preprocessing.R
+++ metabolite_preprocessing.R
@@ -40,7 +40,7 @@ data %>%
   summarise(distinct = n_distinct(value)) %>%
   filter(distinct == 1) %>%
   summarise(n = n())
-## Conclusion: There are 15 constant columns.
+## Conclusion: There are no constant columns.
 
 # Filter the data
 data <- data %>% 
